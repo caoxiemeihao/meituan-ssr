975a38dd0d9991a795787fa2066e2bc284776a3b
草鞋没号 <308487730@qq.com>
2019-10-14 09:35:10 +0800

    upate config.js

--- config.js
+++ config.js
@@ -1,17 +1,33 @@
 
 const path = require('path');
 
+const resolve = p => path.resolve(__dirname, p);
+
 module.exports = {
   apps: {
     admin: {
+      name: 'admin',
       enable: true,
-      entry: path.resolve(__dirname, 'apps/admin'),
+      entry: resolve('apps/admin'),
       port: 8080,
+      version: 1,
     },
     meishi: {
+      name: 'meishi',
       enable: true,
-      entry: path.resolve(__dirname, 'app/meishi'),
+      entry: resolve('apps/meishi'),
       port: 8081,
+      version: 1,
     }
-  }
+  },
+
+  databases: {
+    web: {
+      hose: 'localhost',
+      port: 3306,
+      user: 'root',
+      password: '12345678',
+      database: 'meituan',
+    },
+  },
 };
