8566ca2a0129ea302a37fd082847791e7e328801
草鞋没号 <308487730@qq.com>
2019-10-20 06:17:19 +0800

    .keys 文件抽离

--- libs/serverInc/session.js
+++ libs/serverInc/session.js
@@ -1,11 +1,13 @@
+const fs = require('fs');
+const path = require('path');
 const session = require('koa-session');
 const { main } = require('@mods/__redis');
 
 module.exports = async function(app) {
-  app.keys = [
-    'LJDLJLENkdlfjl#$*&KLjfjldff!@3123',
-    '*&$hfhtg%h^j4$njdffrjdf123nvoDOWF',
-  ];
+  app.keys = fs.readFileSync(path.join(__dirname, '../../\.keys'), 'utf8')
+    .split(/\r\n|\n/).filter(_ => _);
+
+  console.log(app.keys);
 
   app.use(session({
     maxAge: (1000 * 60 * 60) * 2, // 2 小时 (PHP 默认 20 分钟)
@@ -13,5 +15,16 @@ module.exports = async function(app) {
     renew: true, // 自动延期
     httpOnly: true, // 不让前端修改 (cookie)
     signed: true, // cookie 签名，防止修改
+    store: {
+      async get(key, maxAge) {
+
+      },
+      async set(key, session, maxAge) {
+
+      },
+      async destroy(key) {
+
+      },
+    },
   }, app));
 };
